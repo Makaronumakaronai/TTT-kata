6b230909b53b0677b7a7df1ea4f0da634fc4516a
Ed Maciulis <edvinas.maciulis@bnymellon.com>
2014-08-07 10:31:22 +0100

    refactoring

--- src/main/java/uk/co/ed/GameEngine.java
+++ src/main/java/uk/co/ed/GameEngine.java
@@ -9,11 +9,9 @@ public class GameEngine {
     }
 
     public Winner whoIsTheWinnerGiven(String board) {
-        if(board.startsWith("xxx")) {
-            return Winner.X;
-        }
-        if(board.startsWith("ooo")) {
-            return Winner.O;
+        if(board.charAt(0) == board.charAt(1) && board.charAt(0) == board.charAt(2) &&
+                board.charAt(0) != '.') {
+            return Winner.valueOf(String.valueOf(board.charAt(0)).toUpperCase());
         }
 
         return Winner.NOT_EXIST;

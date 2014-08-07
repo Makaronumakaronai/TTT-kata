f861de57177593e9bff625c04c995567b3f3abcd
Ed <edwin.maciul@gmail.com>
2014-08-07 19:48:32 +0100

    refactoring after passing test - trying to find a winner diagonally from left

--- src/main/java/uk/co/ed/GameEngine.java
+++ src/main/java/uk/co/ed/GameEngine.java
@@ -27,12 +27,16 @@ public class GameEngine {
         if(isThereAWinnerInColumnGiven(board, 2)) {
             return getWinnerGiven(board, 2);
         }
-        if(board.charAt(0) == board.charAt(4) && board.charAt(0) == board.charAt(8) &&
-                board.charAt(0) != '.') {
+        if(isThereAWinnerDiagonallyFromLeft(board)) {
             return getWinnerGiven(board, 0);
         }
 
         return Winner.NOT_EXIST;
+    }
+
+    private boolean isThereAWinnerDiagonallyFromLeft(String board) {
+        return board.charAt(0) == board.charAt(4) && board.charAt(0) == board.charAt(8) &&
+                board.charAt(0) != '.';
     }
 
     private boolean isThereAWinnerInColumnGiven(String board, int columnIndex) {

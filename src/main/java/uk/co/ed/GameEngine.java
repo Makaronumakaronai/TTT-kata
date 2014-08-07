76f625ff006e49f29e125cb81fa1ad93f1a91972
Ed Maciulis <edvinas.maciulis@bnymellon.com>
2014-08-07 10:40:50 +0100

    refactoring after passing test - trying to find winner in second row

--- src/main/java/uk/co/ed/GameEngine.java
+++ src/main/java/uk/co/ed/GameEngine.java
@@ -9,15 +9,22 @@ public class GameEngine {
     }
 
     public Winner whoIsTheWinnerGiven(String board) {
-        if(board.charAt(0) == board.charAt(1) && board.charAt(0) == board.charAt(2) &&
-                board.charAt(0) != '.') {
-            return Winner.valueOf(String.valueOf(board.charAt(0)).toUpperCase());
+        if(isThereAWinnerGivenStartOfARow(board, 0)) {
+            return getWinnerGiven(board, 0);
         }
-        if(board.charAt(3) == board.charAt(4) && board.charAt(3) == board.charAt(5) &&
-                board.charAt(0) != '.') {
-            return Winner.valueOf(String.valueOf(board.charAt(4)).toUpperCase());
+        if(isThereAWinnerGivenStartOfARow(board, 3)) {
+            return getWinnerGiven(board, 3);
         }
 
         return Winner.NOT_EXIST;
+    }
+
+    private boolean isThereAWinnerGivenStartOfARow(String board, int rowStart) {
+        return board.charAt(rowStart) == board.charAt(rowStart+1) && board.charAt(rowStart) == board.charAt(rowStart+2) &&
+                board.charAt(rowStart) != '.';
+    }
+
+    private Winner getWinnerGiven(String board, int secondRowStartIndex) {
+        return Winner.valueOf(String.valueOf(board.charAt(secondRowStartIndex)).toUpperCase());
     }
 }
